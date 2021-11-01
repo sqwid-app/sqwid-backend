54960fa5ee8c58ceedc78bb6166d674da11d3895
andithemudkip <andithemudkip@gmail.com>
2021-11-01 18:26:55 +0200

    fix: change syncing strategy

--- routes/create/collectible.js
+++ routes/create/collectible.js
@@ -26,18 +26,22 @@ let upload = async (req, res, next) => {
     const cover = req.files.coverData ? req.files.coverData [0] : req.files.fileData [0];
     const file = (req.files.coverData && (req.files.coverData [0] === req.files.fileData [0])) ? null : req.files.fileData [0];
 
-    const collection = await firebase.collection ('collections').doc (req.body.collection).get ();
+    let col = req.body.collection || "Sqwid";
+    const collection = await firebase.collection ('collections').doc (col).get ();
 
     if (collection.exists) {
-        if (collection.data ().owner === req.user.address) {
+        if (collection.data ().owner === req.user.address || col === "Sqwid") {
             try {
                 const metadata = await client.store ({
                     name: req.body.name || "Empty Sqwid",
                     description: req.body.description || "",
-                    collection: req.body.collection || "Sqwid",
-                    media: file ? (new File ([file.buffer], file.originalname, { type: file.mimetype })) : null,
-                    properties: JSON.parse (req.body.properties) || {},
-                    mimetype: file ? file.mimetype : null,
+                    properties: {
+                        custom: JSON.parse (req.body.properties) || {},
+                        mimetype: file ? file.mimetype : null,
+                        creator: req.user.address,
+                        media: file ? (new File ([file.buffer], file.originalname, { type: file.mimetype })) : null,
+                        collection: col,
+                    },
                     image: new File (
                         [cover.buffer],
                         cover.originalname,
@@ -60,67 +64,37 @@ let upload = async (req, res, next) => {
     }
 }
 
-let addToDb = async (req, res, next) => {
-    const userAddress = req.user.address;
-    const { metadata } = req.body;
+let sync = async (req, res, next) => {
     const { provider } = await getWallet ();
-
-    let evmAddress = await provider.api.query.evmAccounts.evmAddresses (userAddress);
-    evmAddress = (0, ethers.utils.getAddress) (evmAddress.toString ());
-
     const contract = new ethers.Contract (process.env.COLLECTIBLE_CONTRACT_ADDRESS, ABI, provider);
 
     const currentId = Number (await contract.currentId ());
 
-    for (let i = currentId; i > Math.max (currentId - 10, 0); i--) {
-        const uri = await contract.uri (i);
-        if (uri === metadata) {
-            const owners = await contract.getOwners (i);
-            if (owners.length === 1 && owners [0] === evmAddress) {
-                const dbCollection = firebase.collection ('collectibles');
-                const doc = await dbCollection.where ('id', '==', i).get ();
-                if (doc.empty) {
-                    let url = getCloudflareURL (metadata);
-                    try {
-                        const re = await axios (url);
-                        const json = await re.data;
-                        
-                        const { name, collection } = json;
-                        const data = {
-                            id: i,
-                            uri: metadata,
-                            collection: collection,
-                            createdAt: new Date (),
-                            creator: userAddress,
-                            name: name
-                        };
+    const dbCollection = firebase.collection ('collectibles');
 
-                        await dbCollection.doc (i.toString ()).set (data);
-                        res.status (200).json ({
-                            status: 'success',
-                            id: i
-                        });
-                    } catch (err) {
-                        next (err);
-                    }
-                } else {
-                    res.status (400).json ({
-                        status: 'error',
-                        message: 'Collectible already exists'
-                    });
-                }
-                return;
-            } else {
-                res.status (403).send ('Not allowed');
-                return;
-            }
-        } else {
-            continue;
+    for (let i = currentId; i > Math.max (currentId - 5, 0); i--) {
+        const uri = await contract.uri (i);
+        let url = getCloudflareURL (uri);
+        const doc = await dbCollection.where ('id', '==', i).get ();
+        if (doc.empty) {
+            const response = await axios (url);
+            const json = await response.data;
+            const { name, properties } = json;
+            const { collection, creator } = properties;
+
+            const data = {
+                id: i,
+                uri,
+                collection: collection || "Sqwid",
+                createdAt: new Date (),
+                creator,
+                name
+            };
+            await dbCollection.doc (i.toString ()).set (data);
         }
     }
-    res.status (400).json ({
-        status: 'error',
-        message: 'Collectible not found'
+    res.status (200).json ({
+        message: 'Sync complete.'
     });
 }
 
@@ -128,7 +102,7 @@ module.exports = () => {
     const router = Router ();
 
     router.post ('/', [ verify, mediaUpload.fields ([{ name: 'fileData', maxCount: 1 }, { name: 'coverData', maxCount: 1 }]) ], upload);
-    router.post ('/db', verify, addToDb);
+    router.get ('/sync', sync);
 
     return router;
 }
