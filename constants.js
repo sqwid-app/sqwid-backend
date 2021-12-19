40d16c1408504f4ee4e88b76446dde0ba7b5358b
andithemudkip <andithemudkip@gmail.com>
2021-12-19 17:23:07 +0200

    feat: add constants file

---
+++ constants.js
@@ -0,0 +1,14 @@
+const contracts = {
+    reef_testnet: {
+        marketplace: '0x64c6855Ad6DFB9b9B6f700CfF231066398B46CA6',
+        erc1155: '0xcBfC344bEefED6FEae98F0e2FF4af9580f601C34',
+        utility: '0x5Ba166aC0F513ec08F35CfD661760Db4928b815B',
+        wrapper: '0x7b9eE96d67D4352ce5b7129f11387128BA9c2Db4'
+    },
+    reef_mainnet: {},
+    godwoken: {}
+}
+
+module.exports = {
+    contracts
+}
