d3d5eece506603081b03d5bce321d7d26dd91c98
boo-0x <boo_0x@tutanota.com>
2023-08-25 11:57:55 +0200

    Fetch bids by owner

--- routes/get/marketplace.js
+++ routes/get/marketplace.js
@@ -1,7 +1,6 @@
 const ethers = require ('ethers');
 const { Router } = require ('express');
 const rateLimit = require ('express-rate-limit');
-const utilityContractABI = require ('../../contracts/SqwidUtility').ABI;
 const collectibleContractABI = require ('../../contracts/SqwidERC1155').ABI;
 const marketplaceContractABI = require ('../../contracts/SqwidMarketplace').ABI;
 const multicallContractABI = require ('../../contracts/Multicall3').ABI;
@@ -10,17 +9,15 @@ const getNetwork = require ('../../lib/getNetwork');
 const firebase = require ('../../lib/firebase');
 const { FieldPath } = require ('firebase-admin').firestore;
 const { fetchCachedCollectibles, cacheCollectibles, fetchCachedCollections, cacheCollections, fetchCachedNames, cacheNames } = require('../../lib/caching');
-const UtilityContract = (signerOrProvider) => new ethers.Contract (getNetwork ().contracts ['utility'], utilityContractABI, signerOrProvider);
 const CollectibleContract = (signerOrProvider, contractAddress) => new ethers.Contract (contractAddress || getNetwork ().contracts ['erc1155'], collectibleContractABI, signerOrProvider);
 const MarketplaceContract = (signerOrProvider) => new ethers.Contract (getNetwork ().contracts ['marketplace'], marketplaceContractABI, signerOrProvider);
 const MulticallContract = (signerOrProvider, contractAddress) => new ethers.Contract (contractAddress || getNetwork ().contracts ['multicall'], multicallContractABI, signerOrProvider);
 const { verify } = require ('../../middleware/auth');
 const { getEVMAddress } = require('../../lib/getEVMAddress');
-const { balanceQuery, doQuery, withdrawableQuery, itemByNftIdQuery, positionsByStateQuery } = require('../../lib/graphqlApi');
-let provider, utilityContract, marketplaceContract, collectibleContract;
+const { balanceQuery, doQuery, withdrawableQuery, itemByNftIdQuery, positionsByStateQuery, bidsByBidder } = require('../../lib/graphqlApi');
+let provider, marketplaceContract, collectibleContract;
 getWallet ().then (async wallet => {
     provider = wallet.provider;
-    utilityContract = UtilityContract (provider);
     marketplaceContract = MarketplaceContract (provider);
     collectibleContract = CollectibleContract (provider);
     multicallContract = MulticallContract (provider);
@@ -59,8 +56,6 @@ const sliceIntoChunks = (arr, chunkSize) => {
 }
 
 const setBoolean = (packedBools, boolNumber, value) => value ? packedBools | 1 << boolNumber : packedBools & ~(1) << boolNumber;
-const maxOfArray = arr => arr.reduce ((a, b) => Math.max (a, b), -Infinity);
-const minOfArray = arr => arr.reduce ((a, b) => Math.min (a, b), Infinity);
 
 const getBoolsArray = arr => {
     const max = arr [arr.length - 1];
@@ -190,45 +185,6 @@ const buildLoanData = (loanData, names) => {
         lender: {
             address: loanData.lender,
             name: names [loanData.lender] || loanData.lender
-        }
-    }
-}
-
-const getSaleData = position => {
-    return {
-        price: Number (position.price)
-    }
-}
-
-const getAuctionData = (item, names) => {
-    return {
-        deadline: Number (item.auctionData.deadline),
-        minBid: Number (item.auctionData.minBid),
-        highestBid: Number (item.auctionData.highestBid),
-        highestBidder: {
-            address: item.auctionData.highestBidder,
-            name: names [item.auctionData.highestBidder] || item.auctionData.highestBidder
-        },
-    }
-}
-
-const getRaffleData = item => {
-    return {
-        deadline: Number (item.raffleData.deadline),
-        totalValue: Number (item.raffleData.totalValue) * (10 ** 18),
-        totalAddresses: Number (item.raffleData.totalAddresses),
-    }
-}
-
-const getLoanData = (item, names) => {
-    return {
-        deadline: Number (item.loanData.deadline),
-        loanAmount: Number (item.loanData.loanAmount),
-        feeAmount: Number (item.loanData.feeAmount),
-        numMinutes: Number (item.loanData.numMinutes),
-        lender: {
-            address: item.loanData.lender,
-            name: names [item.loanData.lender] || item.loanData.lender
         }
     }
 }
@@ -456,36 +412,6 @@ const buildObjectsFromPositions = async (positions, additionalNamesSearch) => {
     }
 }
 
-const buildObjectsFromItems = async (items) => {
-    const itemIds = Array.from (new Set (items.map (item => Number (item.item.itemId))));
-    const collectionsSet = new Set (items.map (item => collectionsOfApprovedItems [approvedIds.find (i => i.id === Number (item.item.itemId)).collection]));
-    const addresses = new Set (items.reduce ((acc, item) => [...acc, item.owner, item.item.creator, item.loanData.lender, item.auctionData.highestBidder], []));
-    const collectiblesPromise = getDbCollectibles (itemIds);
-    const namesPromise = getNamesByEVMAddresses (Array.from (addresses));
-    const collectionsPromise = getDbCollections (Array.from (collectionsSet));
-    const [collectibles, names, collections] = await Promise.all ([collectiblesPromise, namesPromise, collectionsPromise]);
-
-    const collectiblesObject = collectibles.reduce ((acc, curr) => {
-        acc [curr.id] = curr;
-        return acc;
-    }, {});
-
-    const collectionsObject = collections.reduce ((acc, collection) => {
-        delete collection.data.traits;
-        return { ...acc, [collection.id]: { ...collection.data, id: collection.id } };
-    }, {});
-    let namesObj;
-    names.forEach (name => {
-        namesObj = { ...namesObj, [name.address]: name.name };
-    });
-    
-    return {
-        collectibles: collectiblesObject,
-        collections: collectionsObject,
-        names: namesObj
-    }
-}
-
 const getClaimableItems = async (address) => {
     let claimableItems = await firebase.collection ('claims')
         .where ('claimed', '==', false)
@@ -588,26 +514,6 @@ const grabItemsWithTraits = async (traits, collectionId) => {
     });
     return ids;
 }
-
-const constructAllowedBytes = (collectionId = null, ids = []) => {
-    let allowedBytes = [];
-    if (ids.length) allowedBytes = getBoolsArray (ids);
-    else if (collectionId) {
-        let idsInCollection = [];
-        for (let i = 0; i < approvedIds.length; i++) {
-            if (collectionsOfApprovedItems [approvedIds [i].collection] === collectionId) {
-                idsInCollection.push (approvedIds [i].id);
-            }
-        }
-        if (idsInCollection.length > 0) allowedBytes = getBoolsArray (idsInCollection);
-        else return [];
-    } else {
-        if (approvedIds.length > 0) allowedBytes = getBoolsArray (approvedIds.map (item => item.id));
-        else return [];
-    }
-    return allowedBytes;
-}
-
 
 const fetchSummary = async (_req, res) => {
     try {
@@ -911,14 +817,16 @@ const fetchBidsByOwner = async (req, res) => {
     const page = Number (req.query.page) || 1;
     const pageSize = Number (req.query.pageSize) || 10;
     try {
-        // TODO
-        const response = await utilityContract.fetchAddressBidsPage (evmAddress, pageSize, page, true);
-        const metas = await getDbCollectibles (response.bids.map (item => item.auction.item.itemId));
-        let bids = response.bids.map ((item, i) => {
+        const response = await doQuery (bidsByBidder (
+            evmAddress,
+            (page - 1) * pageSize,
+            pageSize
+        ));
+        
+        const metas = await getDbCollectibles (response.bids.map (bid => bid.itemId));
+        let bids = response.bids.map ((bid, i) => {
             return {
-                positionId: Number (item.auction.positionId),
-                amount: Number (item.auction.amount),
-                bidAmount: Number (item.bidAmount) / 10 ** 18,
+                ...bid,
                 meta: metas [i]
             };
         });
@@ -926,7 +834,7 @@ const fetchBidsByOwner = async (req, res) => {
         res.status (200).json ({
             bids,
             pagination: {
-                totalPages: response.totalPages.toNumber (),
+                totalPages: Math.ceil(response.totalCount / pageSize),
                 page,
                 pageSize
             }
