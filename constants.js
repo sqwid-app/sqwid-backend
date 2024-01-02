8fca1382f82a60f912ab00fa7ed31f413b3af2fd
matjazonline <matjazonline@gmail.com>
2024-01-02 12:27:38 +0100

    cleanup

--- constants.js
+++ constants.js
@@ -1,11 +1,11 @@
 
 const networks = {
     'reef_testnet': {
-        rpc: 'wss://rpc-testnet.reefscan.info/ws',
+        rpc: 'wss://rpc-testnet.reefscan.com/ws',
         contracts: {
             marketplace: '0x31939DF5c6A5ac0b574EDE6E610Fd30c08788A53',
-	    erc1155: '0x9FdEb478A27E216f80DaEE0967dc426338eD02f2',
-	    utility: '0x8E7Ef6bD28cD9bDb6DBf105140958ac03EeC371A',
+            erc1155: '0x9FdEb478A27E216f80DaEE0967dc426338eD02f2',
+            utility: '0x8E7Ef6bD28cD9bDb6DBf105140958ac03EeC371A',
             multicall: '0x399d847d3D8F6b9F9A30e4Dc9C89F65a4EF65821'
         },
         typesense: {
@@ -21,7 +21,7 @@ const networks = {
         graphql_api_marketplace: 'https://squid.subsquid.io/sqwid-marketplace-testnet/graphql',
     },
     'reef_mainnet': {
-        rpc: 'wss://rpc.reefscan.info/ws',
+        rpc: 'wss://rpc.reefscan.com/ws',
         contracts: {
             marketplace: "0xB13Be9656B243600C86922708C20606f5EA89218",
 	    erc1155: "0x0601202b75C96A61CDb9A99D4e2285E43c6e60e4",
--- ecosystem.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  apps : [{
-    script: 'index.js',
-    name: 'sqwid-api',
-    instances: 'max'
-  }]
-};
