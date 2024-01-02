7521888e2e3b2b7c99a4a1842ee46f8eaffb25b3
matjazonline <matjazonline@gmail.com>
2024-01-02 13:28:59 +0100

    log error

--- scripts/cleanTempUploads.js
+++ scripts/cleanTempUploads.js
@@ -4,11 +4,15 @@ const { TEMP_PATH } = require("../constants");
 const INTERVAL = 1000 * 60 * 30; // 30 minutes
 
 const cleanTempUploads = () => {
-    const files = fs.readdirSync(TEMP_PATH);
-    for (const file of files) {
-        if (fs.statSync(TEMP_PATH + file).mtime < Date.now() - INTERVAL) {
-            fs.rmSync(`${TEMP_PATH}/${file}`, {recursive: true});
+    try {
+        const files = fs.readdirSync(TEMP_PATH);
+        for (const file of files) {
+            if (fs.statSync(TEMP_PATH + file).mtime < Date.now() - INTERVAL) {
+                fs.rmSync(`${TEMP_PATH}/${file}`, {recursive: true});
+            }
         }
+    }catch (e) {
+        console.log('ERROR cleanTempUploads=',e.message);
     }
 };
 
