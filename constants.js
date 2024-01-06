e43aecd6da6d91d0545ef1336b2c2bcddb737888
Anukul Pandey <anukul.030601@gmail.com>
2024-01-06 19:22:02 +0530

    feat: removed pusher

--- constants.js
+++ constants.js
@@ -50,16 +50,12 @@ const config = {
             nftContractAddress: '0xc2F3BE4636A0a1ddf3b4D63ef22014DD41114336',
             explorerGraphqlUrl: 'https://squid.subsquid.io/reef-explorer-testnet/graphql',
             marketplaceGraphqlUrl: 'https://squid.subsquid.io/sqwid-marketplace-testnet/graphql',
-            pusherEventExplorer: 'block-finalised-testnet',
-            // pusherEventMarketplace: 'sqwid-events-emitted-testnet'
           },
           mainnet: {
             marketplaceContractAddress: '0xB13Be9656B243600C86922708C20606f5EA89218',
             nftContractAddress: '0x0601202b75C96A61CDb9A99D4e2285E43c6e60e4',
             explorerGraphqlUrl: 'https://squid.subsquid.io/reef-explorer/graphql',
             marketplaceGraphqlUrl: 'https://squid.subsquid.io/sqwid-marketplace/graphql',
-            pusherEventExplorer: 'block-finalised',
-            // pusherEventMarketplace: 'sqwid-events-emitted'
           }
     }
 
