d8631dd759c23aca4d17ca59f7a4c4a355fbaf48
matjazonline <matjazonline@gmail.com>
2024-01-17 01:06:39 +0100

    remove cors

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const cors = require ('cors');
 app.set ('trust proxy', 2);
 app.use (morgan ('dev'));
 // app.use (helmet ());
-app.use (cors ());
+// app.use (cors ());
 app.use (express.json ({ limit: "100mb" }));
 app.use (express.urlencoded ({extended: true, limit: "100mb"}));
 app.use (express.raw ({ type: "application/octet-stream", limit: "50mb" }));
