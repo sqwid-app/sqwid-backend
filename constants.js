5d1950bd7e423bed1779aca77a473d6bb050344d
matjazonline <matjazonline@gmail.com>
2024-01-20 00:32:16 +0100

    fix contract addresses

--- constants.js
+++ constants.js
@@ -48,8 +48,8 @@ const TEMP_PATH = "./temp-uploads/";
 
 const config = {
         testnet: {
-            marketplaceContractAddress: '0xB4630116Dca95650C1E56F3dD39c7edeb1075B38',
-            nftContractAddress: '0xc2F3BE4636A0a1ddf3b4D63ef22014DD41114336',
+            marketplaceContractAddress: '0x31939DF5c6A5ac0b574EDE6E610Fd30c08788A53',
+            nftContractAddress: '0x9FdEb478A27E216f80DaEE0967dc426338eD02f2',
             explorerGraphqlUrl: 'https://squid.subsquid.io/reef-explorer-testnet/graphql',
             marketplaceGraphqlUrl: 'https://squid.subsquid.io/sqwid-marketplace-testnet/graphql',
           },
